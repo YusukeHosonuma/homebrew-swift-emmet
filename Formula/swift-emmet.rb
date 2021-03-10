d6f5bb75dcce9be57222fc082824d7aed6b85da2
GitHub Action <action@github.com>
2021-03-10 08:01:16 +0000

    update version

--- Formula/swift-emmet.rb
+++ Formula/swift-emmet.rb
@@ -1,13 +1,10 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SwiftEmmet < Formula
   desc "A CLI tool that generate struct or class by simple-syntax"
   homepage "https://github.com/YusukeHosonuma/swift-emmet"
-  url "https://github.com/YusukeHosonuma/swift-emmet/releases/download/0.1.0/swift-emmet"
-  sha256 "8c05f5a797d2ce021a40dc351b86a2ff94f10e901bbf98bb3c53888b3f22897d"
+  url "https://github.com/YusukeHosonuma/swift-emmet/releases/download/v0.2.0/swift-emmet"
+  sha256 "0d4ebfa00867d2e67f325f8add1b9c6f0f5e51313f1d73fe3f5af3c81516dc93"
   license "MIT"
-
+  
   def install
     bin.install "swift-emmet"
   end
